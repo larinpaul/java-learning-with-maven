f1d76485cd62f8c4021c20d858067fc1d4aec487
Pavel Larin <larinpavelfirst@mail.ru>
2023-05-06 11:26:00 +0500

    Copy objects in Java

---
+++ src/main/java/org/example/copyobjects/Main.java
@@ -0,0 +1,30 @@
+package org.example.copyobjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Chevrolet", "Camaro", 2021);
+//        Car car2 = new Car("Ford", "Mustang", 2022);
+//        car2.copy(car1);
+
+        // car2 = car1; // Don't do that... It makes them the same exact car
+
+        // Let's create a copy constructor
+        Car car2 = new Car(car1);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println();
+        System.out.println(car1.getMake());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getYear());
+        System.out.println();
+        System.out.println(car2.getMake());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getYear());
+
+    }
+
+
+}
